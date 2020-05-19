70cefb9c4aeedab558c2564643d95057bed9871f
Till Schander <tillschander@gmx.de>
2020-05-19 22:42:01 +0200

    CRUD

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,18 +4,21 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PostRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/post")
+ */
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post_index")
+     * @Route("/", name="post_index", methods={"GET"})
      */
-    public function index(PostRepository $postRepository)
+    public function index(PostRepository $postRepository): Response
     {
         return $this->render('post/index.html.twig', [
             'posts' => $postRepository->findAll(),
@@ -23,15 +26,16 @@ class PostController extends AbstractController
     }
 
     /**
-     * @Route("/post/new", name="post_new")
+     * @Route("/new", name="post_new", methods={"GET","POST"})
      */
-    public function new(Request $request, EntityManagerInterface $entityManager)
+    public function new(Request $request): Response
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($post);
             $entityManager->flush();
 
@@ -39,7 +43,52 @@ class PostController extends AbstractController
         }
 
         return $this->render('post/new.html.twig', [
-            'form' => $form->createView()
+            'post' => $post,
+            'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="post_show", methods={"GET"})
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="post_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Post $post): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('post_index');
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="post_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Post $post): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('post_index');
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -4,9 +4,6 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +12,9 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('created_at', DateTimeType::class)
-            ->add('content', TextareaType::class)
+            ->add('title')
+            ->add('created_at')
+            ->add('content')
         ;
     }
 
